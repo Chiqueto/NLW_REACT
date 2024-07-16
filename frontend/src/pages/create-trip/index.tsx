5c87ea40f161f9db44e4f9b16c075db126c373d0
Luis Felipe <83621659+Chiqueto@users.noreply.github.com>
2024-07-16 19:53:08 +0000

    Aula 2 PROPS

---
+++ frontend/src/pages/create-trip/index.tsx
@@ -0,0 +1,132 @@
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { InviteGuestsModal } from './invite-guests-modal'
+import "./../../index.css"
+import { ConfirmTripModal } from './confirm-trip-modal'
+import { DestinationAndDateStep } from './steps/destination-and-date-step'
+import { InviteGuestsStep } from './steps/invite-guests-step'
+
+export function CreateTripPage() {
+  const navigate = useNavigate()
+
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    'lfchiqueto@gmail.com'
+  ])
+
+  function openGuestsInputs(){
+    setIsGuestsInputOpen(true)
+  }
+
+  function closeGuestsInputs(){
+    setIsGuestsInputOpen(false)
+  }
+
+  function openGuestsModal(){
+    setIsGuestsModalOpen(true)
+  }
+
+  function closeGuestsModal(){
+    setIsGuestsModalOpen(false)
+  }
+
+  function openConfirmTripModal(){
+    setIsConfirmTripModalOpen(true)
+  }
+
+  function closeConfirmTripModal(){
+    setIsConfirmTripModalOpen(false)
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    if(!email){
+      return
+    }
+
+    if(emailsToInvite.includes(email)){
+      return
+    }
+
+    setEmailsToInvite([// imutabilidade, elementos não são alterados, então você cria um novo usando os antigos... 
+      ...emailsToInvite,
+      email
+    ])
+
+    event.currentTarget.reset()
+  }
+
+  function removeEmailsFromInvites(emailToRemove: string){
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+
+    setEmailsToInvite(newEmailList)
+  }
+
+  function createTrip(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    navigate('/trips/123')
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center bg-zinc-950">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10 " >
+        <div className='space-y-4'>
+          <div className='flex flex-col items-center'>
+            <img src="./Logo.svg" alt="Plann.er" />
+            <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+          </div>
+
+          <DestinationAndDateStep 
+            closeGuestsInput={closeGuestsInputs}
+            isGuestsInputOpen={isGuestsInputOpen}
+            openGuestsInput={openGuestsInputs}
+          />
+
+          {isGuestsInputOpen && (
+            <InviteGuestsStep 
+                emailsToInvite={emailsToInvite}
+                openConfirmTripModal={openConfirmTripModal}
+                openGuestsModal={openGuestsModal}
+            />
+          )}
+          
+          <p className="text-sm text-zinc-500">
+            Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+            com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade</a>.
+          </p>
+        </div>
+
+      </div>
+
+
+      {isGuestsModalOpen && (
+        <InviteGuestsModal 
+            emailsToInvite={emailsToInvite}
+            addNewEmailToInvite={addNewEmailToInvite}
+            closeGuestModal={closeGuestsModal}
+            removeEmailsFromInvites={removeEmailsFromInvites}
+        />
+      )}
+
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal 
+        closeConfirmTripModal={closeConfirmTripModal}
+        createTrip={createTrip}
+        />
+      )}
+
+    </div>
+  )
+
+
+  
+}
+
+
